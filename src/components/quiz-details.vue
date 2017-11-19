6d3df0f125025436a6e919b33831cbb526cf49a2
Marek Jakimiuk <marek.jakimiuk@pagepro.pl>
2017-11-19 12:31:52 +0100

    #1

---
+++ src/components/quiz-details.vue
@@ -0,0 +1,24 @@
+<template>
+  <section v-if="quiz">
+    <h2> {{ quiz.name }}</h2>
+    <p>{{ quiz.categoryName}}</p>
+    <div>
+
+      <img :src="quiz.promo"/>
+    </div>
+  </section>
+
+</template>
+
+<script>
+export default {
+  name: 'QuizDetails',
+  props: {
+    quiz: Object
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/the-header.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>quiz</h1>
+    <p>
+      Wybierz pytanie?
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader'
+}
+</script>
+
+<style scoped>
+  h1 {
+    background: green;
+  }
+</style>
